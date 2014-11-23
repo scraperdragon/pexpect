79d856b598c1755e3f6cf8d7dd5ce8d19afa02bc
Jeff Quast <contact@jeffquast.com>
2014-11-23 15:18:58 -0800

    add SIGCONT and use select, not time.sleep

--- tests/needs_kill.py
+++ tests/needs_kill.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 """This script can only be killed by SIGKILL."""
 import signal
-import time
+import select
 
 # Ignore interrupt, hangup and continue signals - only SIGKILL will work
 signal.signal(signal.SIGINT, signal.SIG_IGN)
 signal.signal(signal.SIGHUP, signal.SIG_IGN)
+signal.signal(signal.SIGCONT, signal.SIG_IGN)
 signal.signal(signal.SIGTERM, signal.SIG_IGN)
 
+select.select([], [], [], 1)
 print('READY')
-time.sleep(60)
+select.select([], [], [], 60)
